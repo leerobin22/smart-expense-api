f61b2e6188d80e4e7fec6d12930acb57da576998
Robin Lee <leerobin22@gmail.com>
2026-03-01 20:00:37 +0700

    Add login endpoint with JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -31,3 +32,39 @@ export const registerUser = async (req, res) => {
     });
   };
 };
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    };
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+            return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    };
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    })
+  }
+}
