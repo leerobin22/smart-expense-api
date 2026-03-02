2f7cf605c67ee73d34a8d5d989a0c2f6138d481a
Robin Lee <leerobin22@gmail.com>
2026-03-02 22:36:20 +0700

    Add JWT auth middleware and protected routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import healthRoutes from "./routes/health.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import { logger } from "./middleware/logger.middleware.js";
 
 const app = express();
@@ -10,5 +11,6 @@ app.use(logger);
 
 app.use("/health", healthRoutes);
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 export default app;
