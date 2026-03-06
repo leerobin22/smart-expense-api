fb743012916219df84a83fe3e91e2d53d2e968f4
Robin Lee <leerobin22@gmail.com>
2026-03-06 20:26:50 +0700

    Get expenses endpoint with date and category filters

--- src/controllers/expense.controller.js
+++ src/controllers/expense.controller.js
@@ -1,6 +1,9 @@
 import Expense from "../models/expense.model.js";
+import asyncHandler from "express-async-handler";
 
-export const createExpense = async (req, res, next) => {
+const DEFAULT_CATEGORY = ["food", "transport", "shopping", "bills", "others"];
+
+export const createExpense = asyncHandler(async (req, res) => {
   const { merchant, amount, date, category } = req.body;
 
   const expense = await Expense.create({
@@ -15,4 +18,41 @@ export const createExpense = async (req, res, next) => {
     message: "Expense created",
     expense,
   });
-};
+});
+
+export const getExpenses = asyncHandler(async (req, res) => {
+  const { startDate, endDate, category, offset, limit } = req.query;
+
+  const page = parseInt(offset) || 1;
+  const size = parseInt(limit) || 10;
+
+  const filter = {
+    userId: req.user._id,
+  };
+
+  if (category) {
+    if (DEFAULT_CATEGORY.indexOf(category) === -1) {
+      res.status(400);
+      throw new Error("Category not found!");
+    }
+
+    filter.category = category;
+  }
+
+  if (startDate && endDate) {
+    filter.date = {
+      $gte: new Date(startDate),
+      $lte: new Date(endDate),
+    };
+  }
+
+  const expenses = await Expense.find(filter)
+    .sort({ date: -1 })
+    .skip((page - 1) * size)
+    .limit(size);
+
+  res.status(200).json({
+    count: expenses.length,
+    expenses,
+  });
+});
--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createExpense } from "../controllers/expense.controller.js";
+import {
+  createExpense,
+  getExpenses,
+} from "../controllers/expense.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/expenses", protect, createExpense);
+router.get("/expenses", protect, getExpenses);
 
 export default router;
